1b21f92b10a025288f387e9df77ff080a3b362ad
kirill <kirillkukarcev@gmail.com>
2018-10-14 16:35:21 -0400

    updated to work and save data

--- NeuralNetworks/GAN/models/simple/gan.py
+++ NeuralNetworks/GAN/models/simple/gan.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from keras import Sequential, Model
-from keras.layers import Input, InputLayer, Dense,LeakyReLU, Activation, Dropout, Flatten,Reshape
-
+from keras.layers import Input, InputLayer, Dense,LeakyReLU, Activation, Dropout, Flatten,Reshape, BatchNormalization
+from keras.optimizers import Adam
 from keras.datasets import mnist
 
 import os
@@ -25,12 +25,17 @@ class GAN:
         self.epochs = 30000
         self.batch_size = 128
 
+        self.optimizer = Adam(0.0002, 0.5)
+
         self.gen = self.get_generator()
         self.dis = self.get_discriminator()
         self.gan = self.get_combined()
 
         self.g_losses = []
         self.d_losses = []
+        self.accuracy = []
+
+
 
     def get_generator(self):
 
@@ -38,10 +43,13 @@ class GAN:
 
         model.add(Dense(256, input_dim=self.g_input_shape))
         model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(512))
         model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(1024))
         model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(np.prod(self.img_shape), activation='tanh'))
         model.add(Reshape(self.img_shape))
 
@@ -53,19 +61,13 @@ class GAN:
     def get_discriminator(self):
         model = Sequential()
 
+
         model.add(Flatten(input_shape=self.img_shape))
-
-        model.add(Dense(1024, input_dim=784, ))
-        model.add(LeakyReLU(0.2))
-        model.add(Dropout(0.3))
         model.add(Dense(512))
-        model.add(LeakyReLU(0.2))
-        model.add(Dropout(0.3))
+        model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(256))
-        model.add(LeakyReLU(0.2))
-        model.add(Dropout(0.3))
+        model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(1, activation='sigmoid'))
-
         model.summary()
 
         img = Input(shape=self.img_shape)
@@ -76,7 +78,7 @@ class GAN:
     def get_combined(self):
 
         # First comply dis
-        self.dis.compile(loss='binary_crossentropy', optimizer='adam',
+        self.dis.compile(loss='binary_crossentropy', optimizer=self.optimizer,
                          metrics=['accuracy'])
 
 
@@ -94,7 +96,7 @@ class GAN:
         # The combined model (stacked generator and discriminator)
         # Trains the generator to fool the discriminator
         combined = Model(gan_input, validity)
-        combined.compile(loss='binary_crossentropy', optimizer='adam')
+        combined.compile(loss='binary_crossentropy', optimizer=self.optimizer)
 
         combined.summary()
 
@@ -106,7 +108,7 @@ class GAN:
         (X_train, _), (_, _) = mnist.load_data()
 
         # Rescale -1 to 1
-        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+        X_train = X_train / 127.5 - 1.
         X_train = np.expand_dims(X_train, axis=3)
 
         # Adversarial labels
@@ -119,62 +121,95 @@ class GAN:
 
         for epoch in range(self.epochs):
 
-            print("-" * 15, 'Epoch %d' % epoch, "-" * 15)
+            #print("-" * 15, 'Epoch %d' % epoch, "-" * 15)
 
-            for _ in tqdm(range(len(X_train) // self.batch_size)):
+            #for _ in tqdm(range(len(X_train) // self.batch_size)):
 
-                # ---------------------
-                #  Preparation stage
-                # ---------------------
+            # ---------------------
+            #  Preparation stage
+            # ---------------------
+            # Select a random batch of images
+            idx = np.random.randint(0, X_train.shape[0], self.batch_size)
+            imgs = X_train[idx]
+            # Generate a batch of fake images
+            noise = np.random.normal(0, 1, (self.batch_size, self.g_input_shape))
+            gen_imgs = self.gen.predict(noise)
 
-                # Select a random batch of images
-                idx = np.random.randint(0, X_train.shape[0], self.batch_size)
-                imgs = X_train[idx]
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+            # First real data, then fake data
+            self.dis.trainable = True
+            d_loss_real = self.dis.train_on_batch(imgs, valid)
+            d_loss_fake = self.dis.train_on_batch(gen_imgs, fake)
+            d_loss = d_loss_fake+d_loss_real
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+            # Train the generator (to have the discriminator label samples as valid)
+            self.dis.trainable = False
+            noise = np.random.normal(0, 1, (self.batch_size, self.g_input_shape))
+            g_loss = self.gan.train_on_batch(noise, valid)
 
-                # Generate a batch of fake images
-                noise = np.random.normal(0, 1, (self.batch_size, self.g_input_shape))
-                gen_imgs = self.gen.predict(noise)
-
-                #X = np.concatenate([imgs, gen_imgs])
-
-                # Labels for generated and real data
-                #yDis = np.zeros(2 * self.batch_size)
-                # One-sided label smoothing
-                #yDis[:self.batch_size] = 0.9
-
-                # ---------------------
-                #  Train Discriminator
-                # ---------------------
-
-                # First real data, then fake data
-                self.dis.trainable = True
-                d_loss_real = self.dis.train_on_batch(imgs, valid)
-                d_loss_fake = self.dis.train_on_batch(imgs, fake)
-                d_loss = d_loss_fake+d_loss_real
+            # Plot the progress
+            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
 
 
-                # ---------------------
-                #  Train Generator
-                # ---------------------
-
-                # Train the generator (to have the discriminator label samples as valid)
-                self.dis.trainable = False
-                noise = np.random.normal(0, 1, (self.batch_size, self.g_input_shape))
-                g_loss = self.gan.train_on_batch(noise, valid)
-
-            # Store loss of most recent batch from this epoch
-            self.g_losses.append(g_loss)
-            self.d_losses.append(d_loss)
-
-            if epoch % 2 == 0:
-                self.plot_generated_images(epoch)
-                self.save_models(epoch)
+            if epoch % 100 == 0:
+                # Store loss of most recent batch from this epoch
+                self.g_losses.append(g_loss)
+                self.d_losses.append(d_loss)
+                self.accuracy.append(100*d_loss[1])
+                #self.plot_generated_images(epoch)
+                #self.save_models(epoch)
+                self.sample_images(epoch)
+                self.save_graphics(epoch)
                 #Plot the progress
                 print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
 
     def save_models(self, epoch):
         self.gen.save('models/gan_generator_epoch_%d.h5' %epoch)
         self.dis.save('models/gan_discriminator_epoch_%d.h5' % epoch)
+
+    def sample_images(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.g_input_shape))
+        gen_imgs = self.gen.predict(noise)
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/%d.png" % epoch)
+        plt.close()
+
+    def save_graphics(self,epoch):
+
+        plt.figure(1)
+
+        # Plot accurancy
+        plt.subplot(221)
+        plt.title("Accurancy")
+        plt.plot(self.accuracy)
+
+        # Plot G_loss
+        plt.subplot(222)
+        plt.title("G_loss")
+        plt.plot(self.g_losses)
+
+        # Plot D_loss
+        plt.subplot(223)
+        plt.title("D_loss")
+        plt.plot(self.d_losses)
+
+        plt.tight_layout()
+        plt.savefig('graphs/graphs_%d.png' % epoch)
 
     def plot_generated_images(self, epoch, examples=100, dim=(10,10), figsize=(10,10)):
 
