cdd1cf0bfe61ddfdc0af6eeb2d6d32a33d4c17a8
kirill <kirillkukarcev@gmail.com>
2018-10-28 15:23:30 +0300

    ReLu accuracy: 0.9769

--- NeuralNetworks/Practice/Practice2/handwritten_number_detector_v2.py
+++ NeuralNetworks/Practice/Practice2/handwritten_number_detector_v2.py
@@ -8,15 +8,18 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 # Input
 x = tf.placeholder(tf.float32, [None, 784],name='input')
 
-W = tf.Variable(tf.zeros([784, 10]), name='weight')
-b = tf.Variable(tf.zeros([10]), name='bias')
+keep_probability = tf.placeholder(tf.float32,name='prob')
 
-relu = tf.nn.relu(tf.matmul(x, W) + b,)
+W_1 = tf.Variable(tf.truncated_normal([784, 784],stddev=0.1), name='weight_1')
+b_1 = tf.Variable(tf.truncated_normal([784],stddev=0.1), name='bias_1')
 
-keep_probability = tf.placeholder(tf.float32,name='keep_probability')
-dropout = tf.nn.dropout(relu, keep_probability)
+hidden_layer_1 = tf.layers.dropout(tf.nn.relu(tf.matmul(x, W_1) + b_1))
 
-y = tf.nn.sigmoid(dropout)
+
+W_2 = tf.Variable(tf.zeros([784, 10]), name='weight_2')
+b_2 = tf.Variable(tf.zeros([10]), name='bias_2')
+
+y = tf.nn.softmax(tf.matmul(hidden_layer_1, W_2) + b_2)
 
 y_ = tf.placeholder(tf.float32, [None, 10],name='class')
 
@@ -27,18 +30,19 @@ train_step = tf.train.GradientDescentOptimizer(0.5, name='optimizer').minimize(c
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
+             #feed_dict={keep_probability:0.5})
 
-    for i in range(1000):
+    for i in range(2000):
             batch_xs, batch_ys = mnist.train.next_batch(100)
             sess.run(train_step, feed_dict={x: batch_xs,
                                             y_: batch_ys,
-                                            keep_probability:0.5})
+                                           })
 
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print("Accuracy: %s" % sess.run(accuracy, feed_dict={x: mnist.test.images,
                                                          y_: mnist.test.labels,
-                                                         keep_probability: 0.5
+
                                                          }))
 
     file_writer = tf.summary.FileWriter('board/linear_regress', sess.graph)
